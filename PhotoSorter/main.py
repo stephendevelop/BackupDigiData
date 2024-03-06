7326f37317ecf9e69e0b1d7e93db75d05d4c0d6d
kempenas <kempenas@P200122.vito.local>
2024-03-06 10:16:02 +0100

    update

--- PhotoSorter/main.py
+++ PhotoSorter/main.py
@@ -29,7 +29,7 @@ FOLDER_TO_INSPECT = 'D:\\GoogleBackup\\28122021\\Kids'
 FOLDER_TO_INSPECT = 'F:\\'
 FOLDER_TO_INSPECT = 'F:\\els fotos 2019'
 FOLDER_TO_INSPECT = 'D:\\PhotosMoviesBackup\\Movies'
-FOLDER_TO_INSPECT = 'D:\\GoogleBackup\\WorkFolder\\Kids_01022023'
+FOLDER_TO_INSPECT = 'D:\\GoogleBackup\\WorkFolder\\Kids_20112023'
 
 
 def print_hi(name):
